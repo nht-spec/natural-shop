a69c22a4e4c7d4eadb25e1756c3f2a0105c82dd2
hoang thinhh <hoangthinhh.vn44@gmail.com>
2021-05-06 17:53:06 +0700

    push

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import MessengerCustomerChat from 'react-messenger-customer-chat';
 import { Route, Switch } from "react-router";
 import CategoryFeature from "./features/Category";
 import Header from "./features/header";
+import HomeNatural from './features/HomeNatural';
 import ProductFeature from "./features/Product";
 import HomeFeatures from "./features/ProductforHome";
 
@@ -13,6 +14,7 @@ function App() {
         <Route path='/products' component={ProductFeature}/>
         <Route path='/category' component={CategoryFeature}/>
         <Route path='/productforhome' component={HomeFeatures}/>
+        <Route path= '/' exact component={HomeNatural}/>
       </Switch>
       <MessengerCustomerChat   pageId="103977878530075"  appId="1658852460972444"/>
     </div>
